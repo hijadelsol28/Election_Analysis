49fe53961fdbd3005dd61da40b2992d233656d22
hijadelsol28 <fariasda08@gmail.com>
2022-12-19 20:00:42 -0700

    Adding PyPoll.py file

---
+++ ##Python if-else statement practice.py
@@ -0,0 +1,7 @@
+##Python if-else statement practice
+temperature = int(input("What is the temperature outside? "))
+if temperature >80:
+    print("Turn on the AC!")
+else:
+    print("Open the windows.")
+    
---
+++ Python_practice.py
@@ -0,0 +1,65 @@
+counties = ["Arapahoe", "Denver", "Jefferson"]
+#'If statement'
+if counties [1] == 'Denver':
+    print(counties[1])
+#If else statement
+if "El Paso" in counties:
+    print("El Paso is in the list of counties.")
+else:
+    print("El Paso is not in the list of counties.")
+#Logical Operators-AND
+if "Arapahoe" in counties and "El Paso" in counties:
+    print("Arapahoe and El Paso are in the list of counties.")
+else:
+    print("Arapahoe or El Paso are not in the list of counties.")
+##Logical Operators-OR
+if "Arapahoe" in counties or "El Paso" in counties:
+    print("Arapahoe or El Paso are in the list of counties.")
+else:
+    print("Arapahoe and El Paso are not in the list of counties.")
+#Test Question
+if "Arapahoe" in counties and "El Paso" not in counties:
+   print("Only Arapahoe is in the list of counties.")
+else:
+    print("Arapahoe is in the list of counties and El Paso is not in the list of counties.")
+#For loop
+for county in counties:
+    print(county)
+for i in range(len(counties)):
+    print(counties[i])
+#Get keys of a dictionary
+counties_dict = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
+for county in counties_dict.keys():
+    print(county)
+for county in counties_dict:
+    print(counties_dict.get(county))
+#Print county and registered voter from counties
+for county, voters in counties_dict.items():
+    print(f"{county} county has {voters} registered voters.")
+
+#Get Key-value Pairs of a dictionary
+for county, voters in counties_dict.items():
+    print(county, voters)
+#Dictionary from list of dictionaries
+voting_data = [{"county":"Arapahoe", "registered_voters": 422829},
+                {"county":"Denver", "registered_voters": 463353},
+                {"county":"Jefferson", "registered_voters": 432438}]
+
+for county_dict in voting_data:
+    print(county_dict)
+for county_dict in voting_data:
+     print(county_dict['registered_voters'])
+for county_dict in voting_data:
+    print(county_dict['county'])
+#Fstring
+my_votes = int(input("How many votes did you get in the election? "))
+total_votes = int(input("What is the total votes in the election? "))
+print(f"I received {my_votes / total_votes * 100}% of the total votes.")
+#Multiline fstring
+candidate_votes = int(input("How many votes did the candidate get in the election? "))
+total_votes = int(input("What is the total number of votes in the election? "))
+message_to_candidate = (
+    f"You received {candidate_votes:,} number of votes. "
+    f"The total number of votes in the election was {total_votes:,}. "
+    f"You received {candidate_votes / total_votes * 100:.2f}% of the total votes.")
+print(message_to_candidate)
