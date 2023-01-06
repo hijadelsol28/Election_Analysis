c79bc2e979b344909589af0d8472b182a993cd20
hijadelsol28 <fariasda08@gmail.com>
2023-01-05 23:03:13 -0700

    cleaning up folders

--- PyPoll.py
+++ PyPoll.py
@@ -1,19 +1,25 @@
 # Add our dependencies.
 import csv
 import os
+
 # Assign a variable to load a file from a path.
 file_to_load = os.path.join("Resources", "election_results.csv")
+
 # Assign a variable to save the file to a path.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
+
 # Initialize a total vote counter.
 total_votes = 0
+
 # Candidate options and candidate votes.
 candidate_options = []
 candidate_votes = {}
+
 # Track the winning candidate, vote count, and percentage.
 winning_candidate = ""
 winning_count = 0
 winning_percentage = 0
+
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
